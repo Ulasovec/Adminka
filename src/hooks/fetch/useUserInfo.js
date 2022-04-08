f6c570ab907c26610078a91235c0c99814dcb936
emage <emage@a.b>
2022-04-08 11:46:55 +0400

    Hook useUserInfo()

--- src/hooks/fetch/useUserInfo.js
+++ src/hooks/fetch/useUserInfo.js
@@ -2,7 +2,6 @@ import {useContext} from "react";
 import {useQuery} from "react-query";
 import {UserContext} from "../../store/context/UserContext";
 import {api} from "../../api/axios-config";
-import axios from "axios";
 
 
 export const useUserInfo = () => {
@@ -20,7 +19,10 @@ export const useUserInfo = () => {
     }, {
         enabled: !!sid
     });
-    const {data: {data: {payload: {user_id, role_id, app_id}}}} = querySession;
+    //const {data: {data: {payload: {user_id, role_id, app_id}}}} = querySession;
+    const user_id = querySession.data?.data?.payload?.user_id;
+    const role_id = querySession.data?.data?.payload?.role_id;
+    const app_id = querySession.data?.data?.payload?.app_id;
 
     // acl_role_get_by_id
     const roleGetReqBody = {
@@ -34,7 +36,38 @@ export const useUserInfo = () => {
     }, {
         enabled: !!sid && !!role_id
     });
-    const {data: {data: {name: role_name, about: role_about, is_active: role_is_active}}} = queryRole;
+    //const {data: {data: {name: role_name, about: role_about, is_active: role_is_active}}} = queryRole;
+    const role_name = queryRole.data?.data?.name;
+    const role_about = queryRole.data?.data?.about;
+    const role_is_active = queryRole.data?.data?.is_active;
 
-    return {querySession, user_id, role_id, app_id, queryRole, role_name, role_about, role_is_active};
+    // acl_user_get_by_id
+    const userGetReqBody = {
+        method: 'acl_user_get_by_id',
+        sid,
+        data: {user_id}
+    }
+    const queryUser = useQuery(['user', sid, user_id], async () => {
+        const response = await api.post('/', userGetReqBody);
+        return response.data;
+    }, {
+        enabled: !!sid && !!user_id
+    });
+    //const {data: {data: {name: user_name, is_active: user_is_active}}} = queryUser;
+    const user_name = queryUser.data?.data?.name;
+    const user_is_active = queryUser.data?.data?.is_active;
+
+    return {
+        querySession,
+        user_id,
+        role_id,
+        app_id,
+        queryRole,
+        role_name,
+        role_about,
+        role_is_active,
+        queryUser,
+        user_name,
+        user_is_active
+    };
 }
--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import {useQuerySession} from "../hooks/fetch/QuerySession";
 import {useQueryRole} from "../hooks/fetch/QueryRole";
+import {useUserInfo} from "../hooks/fetch/useUserInfo";
 
 const AdminPage = () => {
 
-    const querySession = useQuerySession();
-    console.log(querySession)
-    const queryRole = useQueryRole();
+    //const querySession = useQuerySession();
+    //console.log(querySession)
+    //const queryRole = useQueryRole();
+    const {user_name, role_name, role_about} = useUserInfo();
+
     return (
         <div style={{fontSize: '1.75em',textAlign:'center'}}>
-            ||| Admin Role : {queryRole?.data?.data?.name} |||
+            {/*||| Admin Role : {queryRole?.data?.data?.name} |||*/}
+            <p>||| User name: {user_name} |||</p>
+            <p>||| User role: {role_name} |||</p>
+            <p>||| Role description: {role_about} |||</p>
 
         </div>
     );
