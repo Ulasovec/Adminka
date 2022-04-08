a86a57c320f99a184883984e79630bfbdd6a3325
Ulasovec <aa@bb.ru>
2022-04-08 11:26:24 +0400

    logout

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useQuerySession} from "../hooks/fetch/QuerySession";
 import {useQueryRole} from "../hooks/fetch/QueryRole";
+import {Container} from "react-bootstrap";
 
 const AdminPage = () => {
 
@@ -8,10 +9,12 @@ const AdminPage = () => {
     console.log(querySession)
     const queryRole = useQueryRole();
     return (
+        <Container>
         <div style={{fontSize: '1.75em',textAlign:'center'}}>
             ||| Admin Role : {queryRole?.data?.data?.name} |||
 
         </div>
+            </Container>
     );
 };
 
--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import {Container} from "react-bootstrap";
+import BootstrapTable from "../components/BootstrapTable/BootstrapTable";
 
 const UsersPage = () => {
     return (
-        <div style={{fontSize: '1.75em',textAlign:'center'}}>
+        <Container>
+        <h1 style={{fontSize: '1.75em',textAlign:'center'}}>
             ||| UsersPage |||
-        </div>
+        </h1>
+            <BootstrapTable/>
+            </Container>
     );
 };
 
